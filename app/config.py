a652201808fd5009edc709e597e8ae64f71a5412
petr <xmf@inbox.ru>
2021-03-19 11:51:40 +0300

    Added crud_app.service

--- app/config.py
+++ app/config.py
@@ -1,5 +1,11 @@
-import os.path
+import os
 
 SECRET_KEY = 'dfasfj9wer@45635sdG#!#TAY&()@$VCb3563rsdgfsa#$%34'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_URL = f""
+
+# MySql settings
+DB_USER = os.environ.get("DB_USER") or 'myuser'
+DB_PASSWORD = os.environ.get("DB_USER") or '123456'
+DB_HOST = os.environ.get("DB_HOST") or '127.0.0.1'
+DB_PORT = os.environ.get("DB_PORT") or '3306'
+DB_URL = os.environ.get('DB_ULR') or f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/notes"
+
--- app/create_models.py
+++
@@ -1,5 +0,0 @@
-from models.base import Base
-
-
-if __name__ == '__main__':
-    Base.metadata.create_all()
